fd1c1f36e0d7796d74e61e4dfc7dfe4bfafb7202
Alex_Min <alexander.min@gmail.com>
2016-07-14 13:13:03 -0400

    added range of valid floors

--- elevator.rb
+++ elevator.rb
@@ -6,7 +6,7 @@ class Elevator
 	end
 
 	def moveToLevel(level)
-		if (@floor > level)
+		if (@floor > level and @floor < 20)
 			print "Going down to floor ", level, "\n"
 			@floor = level
 		elsif (@floor < level)
@@ -18,16 +18,40 @@ class Elevator
 		end
 	end
 
+	def up
+		if @floor == 20
+			print "Already at top floor!\n"
+		else
+			@floor += 1
+		end
+	end
+
+	def down
+		if @floor == 0
+			print "Already at bottom-most floor.\n"
+		else
+			@floor -= 1
+		end
+	end
+
 	def greet
 		print "Hello, you are currently at floor ", @floor, "!\n"
 	end 
 end
 
+print "----------------------\n"
 elev = Elevator.new
 elev.greet
 elev.moveToLevel(9)
 elev.greet
 print elev.floor, "\n"
-
+print "----------------------\n"
 elev.floor = 20
+elev.greet
+print "----------------------\n"
+elev.up
+elev.greet
+print "----------------------\n"
+elev.floor = 0
+elev.down
 elev.greet
